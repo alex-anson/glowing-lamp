89580597cb4fcce4a67070e4a87a7a4dda8428d1
Alex Anson <1alex.anson@gmail.com>
2023-04-16 11:51:13 -0400

    style - ss-rendered route

--- src/pages/ss-rendered.tsx
+++ src/pages/ss-rendered.tsx
@@ -2,7 +2,7 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const res = await fetch("https://dummyjson.com/products");
+  const res = await fetch("https://dummyjson.com/products?limit=12");
   const data = await res.json();
   return {
     props: { data },
@@ -28,14 +28,16 @@ export default function Page(
 ): JSX.Element {
   const productList = props.data.products as Product[]; // again... don't typecast like this at home.
   return (
-    <div className="mt-10 tracking-wide">
+    <div className="my-10 tracking-wide mx-4">
       <h1 className="text-6xl font-light text-center">server-side rendering</h1>
 
-      <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-8 mt-20 xl:mt-40">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-20 place-items-center">
         {productList.map((product) => {
           return (
             <article key={product.id}>
-              <p>Product name: {product.title}</p>
+              <h2 className="text-2xl text-teal-600">{product.title}</h2>
+              <p className="max-w-xs">{product.description}</p>
+              <p>${product.price}</p>
             </article>
           );
         })}
