f37504172df3c0d48e79d1d0f7a97f0eb664fa08
Alex Anson <1alex.anson@gmail.com>
2023-04-17 09:34:24 -0400

    fix link running full width

--- src/components/WannaSeeLink.tsx
+++ src/components/WannaSeeLink.tsx
@@ -12,13 +12,16 @@ interface Props {
 
 export default function WannaSeeLink(props: Props): JSX.Element {
   return (
-    <Link href={props.href} id={props.id}>
+    <Link
+      href={props.href}
+      id={props.id}
+      className="inline-block max-w-fit mx-auto"
+    >
       <div
-        className={`rounded-lg border border-transparent px-6 py-4 transition-colors text-neutral-200 hover:text-neutral-400 hover:border-neutral-700 hover:bg-neutral-800/30 max-w-fit mx-auto overflow-hidden flex justify-center items-center relative ${styles.wannasee}`}
+        className={`rounded-lg border border-transparent px-6 py-4 transition-colors text-neutral-200 hover:text-neutral-400 hover:border-neutral-700 hover:bg-neutral-800/30 overflow-hidden flex justify-center items-center relative ${styles.wannasee}`}
       >
         <h2 className="text-xl font-semibold text-center">{props.children}</h2>
       </div>
     </Link>
   );
 }
-// link is running full width
